1075a77fe306e8e2751c911090ae39ff3f36c40a
verenisseeyzaguirre <verenisse.eyzaguirre@gmail.com>
2020-09-16 20:44:52 -0500

    Updates

---
+++ precache-manifest.4b3fd12e752913d8da22208a3905a7cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc9ab2195e63ae3109373b1ec345c8d8",
+    "url": "/react-diario-app/index.html"
+  },
+  {
+    "revision": "2a1dd3f65e50d905c506",
+    "url": "/react-diario-app/static/css/main.e6ce90ce.chunk.css"
+  },
+  {
+    "revision": "6d63d087cf197ba356e8",
+    "url": "/react-diario-app/static/js/2.925fb7ae.chunk.js"
+  },
+  {
+    "revision": "d507ff1ca8fe411e4d0052be34a4c7b9",
+    "url": "/react-diario-app/static/js/2.925fb7ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a1dd3f65e50d905c506",
+    "url": "/react-diario-app/static/js/main.8eadbac3.chunk.js"
+  },
+  {
+    "revision": "1db3df0a91fd085af9e2",
+    "url": "/react-diario-app/static/js/runtime-main.d2e4e8ca.js"
+  }
+]);
